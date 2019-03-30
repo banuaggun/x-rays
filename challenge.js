0da6e9939ed705a986a8748eb47fc8e40eb9509b
Banu AĞGÜN <37499235+banuaggun@users.noreply.github.com>
2019-03-30 03:11:58 +0300

    Add files via upload

---
+++ challenge.js
@@ -0,0 +1,72 @@
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+
+  }
+  
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+  
+function openFunc(){
+  document.getElementById("myLink").style.width = "100%";
+}
+
+function closeFunc(){
+  document.getElementById("myLink").style.width = "0%";
+}
+
+function openFunc1(){
+  document.getElementById("myLink1").style.width  = "100%";
+}
+
+function closeFunc1(){
+  document.getElementById("myLink1").style.width  = "0%";
+}
+
+function openFunc2(){
+  document.getElementById("myLink2").style.width  = "100%";
+}
+
+function closeFunc2(){
+  document.getElementById("myLink2").style.width  = "0%";
+}
+
+function openFunc3(){
+  document.getElementById("myLink3").style.width = "100%";
+}
+
+function closeFunc3(){
+  document.getElementById("myLink3").style.width  = "0%";  
+}
+
+function openFunc4(){
+  document.getElementById("myLink4").style.width  = "100%";
+}
+
+function closeFunc4(){
+  document.getElementById("myLink4").style.width  = "0%";
+}
+
+function openFunc5(){
+  document.getElementById("myLink5").style.width  = "100%";
+}
+
+function closeFunc5(){
+  document.getElementById("myLink5").style.width  = "0%";
+}
+
+function openFunc6(){
+  document.getElementById("myLink6").style.width  = "100%";
+}
+
+function closeFunc6(){
+  document.getElementById("myLink6").style.width  = "0%";
+}
+
+function openFunc7(){
+  document.getElementById("myLink7").style.width  = "100%";
+}
+
+function closeFunc7(){
+  document.getElementById("myLink7").style.width  = "0%";
+}
